fd9e4c5a45c3667ac8a1f7ed01593c82aece6343
Joseph-Hernande <141504987+Joseph-Hernande@users.noreply.github.com>
2023-09-15 19:31:11 +0000

    Trabajo

--- Clase6/Trabajo/cliente.py
+++ Clase6/Trabajo/cliente.py
@@ -5,6 +5,9 @@ url = 'https://ominous-space-fishstick-pjrp9j9rx9vgf9pp4-8000.app.github.dev/'
 r = requests.get(url)
 print(r.text)
 
-r = requests.post(url + 'encolar', json={'nombre': 'juan', 'productos': ['manzana', 'pera']})
-print()
+r = requests.post(url + 'encolar', json={'nombre': 'juan', 'productos': ['manzana', 'pera'], "Documento": 12345})
+r = requests.post(url + 'encolar', json={'nombre': 'joseph', 'productos': ['manzana', 'manzana'], "Documento": 12345})
+r = requests.post(url + 'encolar', json={'nombre': 'pedro', 'productos': ['manzana', 'pera'], "Documento": 12345})
+r = requests.post(url + 'encolar', json={'nombre': 'pablo', 'productos': ['manzana', 'mango'], "Documento": 12345})
+print(r.text)
 
